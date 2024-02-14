b52127cb167cfe509354f664c97a261f471a6073
yuange <hzyuange@gmail.com>
2024-02-14 13:39:10 +0800

    a

---
+++ app/src/main/java/com/example/ijnikkkk/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ijnikkkk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.ijnikkkk.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'ijnikkkk' library on application startup.
+    static {
+        System.loadLibrary("ijnikkkk");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'ijnikkkk' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
